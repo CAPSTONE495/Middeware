f323b220ce5d582dea6c70a2dced8661a25ce79d
UkraineBolt <alawrence2211@gmail.com>
2020-04-10 09:47:57 -0400

    clean up some unused code

--- src/main/java/com/example/restservice/database/Database.java
+++ src/main/java/com/example/restservice/database/Database.java
@@ -2,7 +2,6 @@ package com.example.restservice.database;
 
 
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
