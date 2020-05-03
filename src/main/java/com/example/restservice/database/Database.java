4aee82d5c29adf8adbb1225a993b7e7cf8c2cd60
nahyromolina48 <nmolina@oswego.edu>
2020-05-02 23:04:17 -0400

    Updated

--- src/main/java/com/example/restservice/database/Database.java
+++ src/main/java/com/example/restservice/database/Database.java
@@ -10,69 +10,8 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 
+import java.util.ArrayList;
 import java.util.List;
-
-/**
- * database scheme User Table{
- * Primary Key: int User ID
- * String FirstName
- * String LastName
- * String Email
- * boolean/bit Is_Driver
- * int seats
- * boolean/bit Is_Admin
- * char[2] Grade
- * int Total_Rides_Taken
- * int Total_Passengers_Taken
- * double Distance_Traveled double Total_Length_Of_Drives }
- * 
- * Rides Table{//TODO make this table
- * Primary Key: int Ride ID (cant be null)
- * int Driver ID (linked to User ID in User Table) (cant be null)
- * int DestinationBusStopID (linked to values in BusStop Table)
- * DateTime StartDate (cant be null) (DateTime format: MM/dd/YYYY HH:mm:SS)
- * DateTime EndDate (cant be null)
- * int Completed (default is 0)
- * int Cancelled (default is 0)
- * int PickupBusStopID (repeat for 10 columns) (linked to values in BusStop Table)
- * int PassengerID (linked to values in BusStop Table)] (repeat for 15 columns)
- * }
- * 
- * BusStop Table{//TODO make this table
- * Primary Key: int BusStopID
- * String locationName
- * String country
- * String state
- * String city
- * String street
- * String areaCode
- * Boolean Destination
- * int PassengerID (repeat for 15 columns)
- * 
- * }
- * 
- * Passenger Table{//TODO make this table
- * Primary Key: int PassengerID
- * int RiderID (linked to User Table)
- * int RideID (linked to Rides table)
- * DateTime StartDate (DateTime format: MM/dd/YYYY)
- * DateTime EndDate (DateTime format: MM/dd/YYYY)
- * int BusStopID pickupLocation (linked to values in BusStop Table) }
- *
- *
- * method overview
- * Users{
- *     addUser,getUser,updateGrade,changeAdminStatus,changeDriverStatus,isAdmin
- * }
- *
- * rides{
- *
- * }
- *
- * busStops{
- *
- * }
- */
 @Service
 public class Database {
 	ApplicationContext ctx = new AnnotationConfigApplicationContext(connection.class);
@@ -82,17 +21,8 @@ public class Database {
 	public Database() {
 
 	}
-
-
 	/*
-
 	User methods that create or finalize entries---------------------------------------------------------------------------------------------------------------------------------------------
-
-	 */
-	/**
-	 * @param email
-	 * make user and add to db
-	 * affected tables: {User Table}
 	 */
 	public boolean addUser(String email) {
 		try {
@@ -104,15 +34,6 @@ public class Database {
 		}
 	}
 
-	/**
-	 * @param email
-	 * @param firstName
-	 * @param lastName
-	 * addName method, takes email, first name, and last name and returns boolean if
-	 * update worked find user by email add firstName and lastname to column return
-	 * true;
-	 * affected tables: {User Table}
-	 */
 	public boolean addName(String email, String firstName, String lastName) {
 		Query lookup = new Query(Criteria.where("email").is(email));
 		try {
@@ -123,31 +44,21 @@ public class Database {
 			throw new RuntimeException("Error with addName: "+e.getLocalizedMessage());
 		}
 	}
-	/**
-	 * @param email
-	 * TODO fill in this method
-	 * Take an email of user, find user by their email, return their account info in the form of a string
-	 * affected tables: {User Table}
-	*/
 	public String getUserInfo(String email){
-		return null;
+		try {
+			Query lookup = new Query(Criteria.where("email").is(email));
+			Users person = mongoOperation.findOne(lookup, Users.class);
+			return person.toString();
+			
+		}catch(Exception e) {
+			throw new RuntimeException("Error with addName: "+e.getLocalizedMessage());
+		}
 	}
 
 	/*
 
 	User methods that edit values---------------------------------------------------------------------------------------------------------------------------------------------------------
 
-	 */
-
-	/**
-	 *
-	 * @param email
-	 * @param grade
-	 *
-	 * updateGrade method, takes in a string to represent
-	 * grade/type of user and email of user find user, replace grade. If any error
-	 * occurs throw runtime exception
-	 * affected tables: {User Table}
 	 */
 	public void updateGrade(String email, String grade) {
 		try {
@@ -157,19 +68,6 @@ public class Database {
 			throw new RuntimeException("Failed to Update grade: "+e.getLocalizedMessage());
 		}
 	}
-
-
-	/**
-	 *
-	 * @param email
-	 * @param isAdmin
-	 *
-	 * changeAdminStatus method, takes a user's email and a
-	 * boolean that represents their new admin status True for being admin, false
-	 * for being standard user. find user, change admin value to isAdmin
-	 * representation if any error occurs throw runtime exception
-	 * affected tables: {User Table}
-	 */
 	public void changeAdminStatus(String email, boolean isAdmin) {
 		try {
 			Query lookup = new Query(Criteria.where("email").is(email));
@@ -178,19 +76,6 @@ public class Database {
 			throw new RuntimeException("failed to change admin status: "+e.getLocalizedMessage());
 		}
 	}
-
-
-	/**
-	 *
-	 * @param email
-	 * @param isDriver
-	 *
-	 * changeDriverStatus method, takes a user's email and a
-	 * boolean that represents their new driver status True for being driver, false
-	 * for not a driver. find user, change driver value to isDriver representation
-	 * if any error occurs throw runtime exception
-	 * affected tables: {User Table}
-	 */
 	public void changeDriverStatus(String email, boolean isDriver) {
 		try {
 			Query lookup = new Query(Criteria.where("email").is(email));
@@ -199,40 +84,18 @@ public class Database {
 			throw new RuntimeException("Failed to change driver status: "+e.getLocalizedMessage());
 		}
 	}
-
-
-	/**
-	 *
-	 * @param email
-	 * @param seats
-	 * TODO fill in this method
-	 * find the user by email, update seat value with seats.
-	 * affected tables: {User Table}
-	 */
 	public void updateSeats(String email,int seats){
 		try{
-
+			Query lookup = new Query(Criteria.where("email").is(email));
+			mongoOperation.updateFirst(lookup, Update.update("seats", seats), Users.class);
 		}catch(Exception e){
 			throw new RuntimeException("Failed to update seats: "+e.getLocalizedMessage());
 		}
 	}
-
-
 	/*
 
 	User Methods that pull values---------------------------------------------------------------------------------------------------------------------------------------------------
 
-	 */
-
-	/**
-	 *
-	 * @param email
-	 * @return
-	 *
-	 * isAdmin method, takes user's email and returns a
-	 * boolean to tell if the account has admin priv or not find user, read isAdmin
-	 * value, return isAdmin value if any error occurs throw runtime exception
-	 * affected tables: {User Table}
 	 */
 	public boolean isAdmin(String email) {
 		Query lookup = new Query(Criteria.where("email").is(email));
@@ -243,62 +106,55 @@ public class Database {
 			throw new RuntimeException("Error with isAdmin: "+e.getLocalizedMessage());
 		}
 	}
-
-	/**
-	 * TODO fill in method pls
-	 * @param email
-	 * @return
-	 *
-	 * use email to get me the id for the user entry
-	 * (this should never happen but) if you cant find the user just return me null, I will check for it
-	 */
 	public String emailToID(String email){
-		return null;
+		try {
+			Query lookup = new Query(Criteria.where("email").is(email));
+			Users person = mongoOperation.findOne(lookup, Users.class);
+			return person.getId();
+		}catch(Exception e){
+			throw new RuntimeException("Failed to change driver status: "+e.getLocalizedMessage());
+		}
 	}
-
-	/**
-	 * TODO fill in method pls
-	 * @param email
-	 * @return
-	 *
-	 * use email to find user and return isDriver var
-	 */
 	public boolean isDriver(String email){
-		return false;
+		try {
+			Query lookup = new Query(Criteria.where("email").is(email));
+			Users person = mongoOperation.findOne(lookup, Users.class);
+			if(person.getDriverStatus()) {
+				return person.getDriverStatus();
+			}else {
+			return false;
+			}
+		}catch(Exception e){
+			throw new RuntimeException("Failed to change driver status: "+e.getLocalizedMessage());
+		}
 	}
 	/*
 
 	Rides Methods that create or delete rides---------------------------------------------------------------------------------------------------------------------------------------------------
 
 	 */
-
-	/**
-	 *
-	 * @param driverID
-	 * @param startTime
-	 * @param endTime
-	 * @param busStopID
-	 *
-	 * TODO fill in method
-	 * make a ride object with these values. the int values can be set to 0;
-	 * Return the id of entry after its been added
-	 */
-	public String addRide(String driverID, String startTime,String endTime,String busStopID){
-
-		return null;
+	public String addRide(Users driverID, String startDate,String endDate, ArrayList<BusStops> destination){
+		try {
+		Rides ride = new Rides(driverID, startDate, endDate, destination);
+		mongoOperation.save(ride);
+		return ride.getId();
+		}catch(Exception e){
+			throw new RuntimeException("Failed to change driver status: "+e.getLocalizedMessage());
+		}
 	}
-
-	/**
-	 * TODO fill in method pls
-	 * @param rideID
-	 *
-	 * find ride with that rideID
-	 * Remove all passengers if any are there
-	 * empty all queues of riders
-	 * Change active value to false;
-	 */
 	public void deleteRide(String rideID){
-
+		try {
+			Query lookup = new Query(Criteria.where("_id").is(rideID));
+			Rides ride = mongoOperation.findOne(lookup, Rides.class);
+			ArrayList<Users> passengers = ride.getPassengers();
+			for(Users user : passengers) {
+				Query person = new Query(Criteria.where("_id").is(user.getId()));
+				mongoOperation.updateFirst(person,Update.update("pickupID", null), Rides.class);
+			}
+			mongoOperation.findAndRemove(lookup, Rides.class);
+		}catch(Exception e){
+			throw new RuntimeException("Failed to change driver status: "+e.getLocalizedMessage());
+		}
 	}
 
 	/*
@@ -307,37 +163,18 @@ public class Database {
 
 	 */
 
-	/**
-	 * TODO fill in method pls
-	 * @param rideID
-	 * @return
-	 * Find the ride that they want to cancel
-	 * go through active riders, if their start date is on the cancel date then push their startDate a week forward. (must check if start Date is passed end date, if so then remove that person and push person in queue up)
-	 * return array of passengerIDs that lost their ride
-	 */
-	public String[] cancelRide(String rideID,String date){
-
-		return null;
-	}
-
 	/*
 
 	Rides Methods that pull values---------------------------------------------------------------------------------------------------------------------------------------------------
 
 	 */
-
-	/**
-	 * TODO fill in method
-	 * @param driverID
-	 * @return
-	 *
-	 * search for all rides that matches driverID and isActive
-	 * with these rides, regenerate completed entries by pulling info you need
-	 * Return that list
-	 */
-	public List<Rides> getDriverRides(String driverID, boolean isActive){
-
-		return null;
+	public List<Rides> getDriverRides(Users driver){
+		try {
+		Query lookup = new Query(Criteria.where("driverID").is(driver));
+		return mongoOperation.find(lookup, Rides.class);
+		}catch(Exception e) {
+			throw new RuntimeException("Failed to change driver status: "+e.getLocalizedMessage());
+		}
 	}
 
 	/**
@@ -349,8 +186,13 @@ public class Database {
 	 * return the length of pickupBusStopIDs
 	 */
 	public int numOfActivePickUps(String rideID){
-
-		return 0;
+		try {
+			Query lookup = new Query(Criteria.where("_id").is(rideID));
+			Rides ride = mongoOperation.findOne(lookup, Rides.class);
+			return ride.getPickUpBusStop().size();
+			}catch(Exception e) {
+				throw new RuntimeException("Failed to change driver status: "+e.getLocalizedMessage());
+			}
 	}
 
 	/**
@@ -369,12 +211,12 @@ public class Database {
 	 * Go through that list and check if there is an open seat for the user.
 	 * (Note: not enough time now, but its recommenced to make this shit parallel with a fork join of the org list because after enough uses that list is gonna be hug)
 	 *
-	 */
+	 
 	public List<Rides> getSearchedBusStops(String locationName, String time, String country, String state, String city, String address, String areaCode){
 
 		return null;
 	}
-
+*/
 	/*
 
 	BusStops Methods that create BusStops---------------------------------------------------------------------------------------------------------------------------------------------------
@@ -394,103 +236,42 @@ public class Database {
 	 * /TODO fill in method pls
 	 * make a busStop entry and then return its id,
 	 *
-	 */
 	public String addBusStop(String rideID,String location,String time,String country,String state,String city,String street,String areaCode,boolean isDestination){
 
 		return null;
 	}
+	*/
 
-	/*
-
-	BusStops Methods that edit values---------------------------------------------------------------------------------------------------------------------------------------------------
-
-	 */
-
-	/**
-	 *
-	 * @param rideID
-	 * @param busStopID
-	 * TODO make method
-	 * Find the busStop entry by id
-	 * Then set rideID = to rideID
-	 */
-	public void linkRideAndBusStop(String busStopID, String rideID){
-
-	}
-
-
-	/*
-
-	BusStop Methods that read values---------------------------------------------------------------------------------------------------------------------------------------------------
-
-	 */
-
-	/*
-
-	Passengers Methods that create values---------------------------------------------------------------------------------------------------------------------------------------------------
-
-	 */
-
-	/*
-
-	Passengers Methods that update values---------------------------------------------------------------------------------------------------------------------------------------------------
-
-	 */
-
-	/**
-	 * TODO fill in method
-	 * @param riderID
-	 * @param startTime
-	 * @param endTime
-	 * @param pickupID
-	 * (pickupID is linked to a busStop ID)
-	 *
-	 * use pickupID to find ride entry
-	 * Check if ride is active
-	 * check if ride has open slot for passenger
-	 * Note: if false for any of these checks just stop here and return false
-	 * Create Passenger
-	 * Add passenger id to slot in correct location in queue
-	 */
-	public boolean addPassenger(String riderID, String startTime, String endTime, String pickupID){
+	public boolean addPassenger(String riderID, Users pass, String pickupID){
+		try {
+			Query ride = new Query(Criteria.where("_id").is(riderID));
+			Rides new_ride = mongoOperation.findOne(ride, Rides.class);
+			ArrayList<Users> passengers = new_ride.getPassengers();
+			passengers.add(pass);
+			pass.setPickUp(pickupID);
+			mongoOperation.save(new_ride);
+			mongoOperation.save(pass);
+			
+		}catch(Exception e) {
+			throw new RuntimeException("Failed to change driver status: "+e.getLocalizedMessage());
+		}
 
 		return false;
 	}
+	public void removePassenger(String riderID, Users pass, String pickupID){
+		try {
+		Query ride = new Query(Criteria.where("_id").is(riderID));
+		Rides new_ride = mongoOperation.findOne(ride, Rides.class);
+		ArrayList<Users> passengers = new_ride.getPassengers();
+		passengers.remove(pass);
+		new_ride.setPassengers(passengers);
+		pass.setPickUp(pickupID);
+		mongoOperation.save(new_ride);
+		mongoOperation.save(pass);
+		}catch(Exception e){
+			throw new RuntimeException("Failed to change driver status: "+e.getLocalizedMessage());
+		}
 
-	/**
-	 * TODO fill in method
-	 * @param riderID
-	 * @return
-	 *
-	 * find passenger with that rider id
-	 * save the ride id
-	 * find the ride
-	 * Remove passenger from queue
-	 */
-	public void removePassenger(String riderID){
-
-
-	}
-
-	/*
-
-	Passengers Methods that read values---------------------------------------------------------------------------------------------------------------------------------------------------
-
-	 */
-
-	/**
-	 * TODO fill in method
-	 * @param riderID
-	 * @param isActive
-	 * @return
-	 *
-	 * Go through passenger table and get all instances where riderID = riderID
-	 * From that list, regenerate all rides buy using the rideID in Passengers.
-	 * return regenerated ride list
-	 */
-	public List<Rides> getMyRides(String riderID, boolean isActive){
-
-		return null;
 	}
 
 }
